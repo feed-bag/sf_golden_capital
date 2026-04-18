e0166ec3753162a6ed4b958acad1d64df91ec8b5
feed-bag <gartenbergsim@gmail.com>
2026-04-18 15:12:16 -0600

    Apply Midnight Capital glassmorphism theme to Kanban board and custom tab bar

--- force-app/main/default/lwc/homeDashboard/homeDashboard.js
+++ force-app/main/default/lwc/homeDashboard/homeDashboard.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class HomeDashboard extends LightningElement {}
+export default class HomeDashboard extends LightningElement {
+    @track activeTab = 'pipeline';
+
+    get isPipeline() { return this.activeTab === 'pipeline'; }
+    get isMetrics()  { return this.activeTab === 'metrics'; }
+
+    get pipelineTabClass() {
+        return 'hd-tab' + (this.activeTab === 'pipeline' ? ' hd-tab-active' : '');
+    }
+    get metricsTabClass() {
+        return 'hd-tab' + (this.activeTab === 'metrics' ? ' hd-tab-active' : '');
+    }
+
+    handleTabClick(evt) {
+        this.activeTab = evt.currentTarget.dataset.tab;
+    }
+}
